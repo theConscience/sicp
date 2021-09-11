93c7724dfe9a2bdd427b1efa42b81912d7da2830
theConscience <vinton.mail@gmail.com>
2021-09-11 15:18:01 +0300

    add solution for 1.8 exercise

---
+++ sicp/chapter1/08.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+; refactoring of sqrt function with incapsulation of locally-defined functions
+
+(define (square x) (* x x))
+
+(define (cube x) (* x x x))
+
+(define (abs x) (if (< x 0) (- x) x))
+
+(define (average x y) (/ (+ x y) 2))
+
+
+(define (cube-root x)
+
+  (define (improve guess)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+  (define (good-enought? guess)
+    (< (abs (- (cube guess) x)) 0.001))
+
+  (define (cube-iter guess)
+    (if (good-enought? guess)
+        guess
+        (cube-iter (improve guess)))) #| recursion! |#
+
+  (cube-iter 1.0))
