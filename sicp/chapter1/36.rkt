0787f8c4d0df01483a88e7ea87a72e4b8d31d14b
theConscience <vinton.mail@gmail.com>
2021-09-25 02:48:20 +0300

    fix test error in 1.36 excercise

--- sicp/chapter1/36.rkt
+++ sicp/chapter1/36.rkt
@@ -18,17 +18,17 @@
 ; (Учтите, что нельзя начинать fixed-point со значения 1, поскольку это
 ; вызовет деление на log(1) = 0 .)
 
-(define (fixed-point f first-guess)
-  (define tolerance 0.000001)
-  (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
-
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (close-enough? guess next)
-          next
-          (try next))))
-
-  (try first-guess))
+; (define (fixed-point f first-guess)
+;   (define tolerance 0.000001)
+;   (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
+;
+;   (define (try guess)
+;     (let ((next (f guess)))
+;       (if (close-enough? guess next)
+;           next
+;           (try next))))
+;
+;   (try first-guess))
 
 
 ; SOLUTION:
