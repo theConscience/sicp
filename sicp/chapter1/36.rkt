569189776aed9b04ddbfc6fd604e786587fc4fe7
theConscience <vinton.mail@gmail.com>
2021-09-25 01:46:21 +0300

    fix 1.36 excercise solution

--- sicp/chapter1/36.rkt
+++ sicp/chapter1/36.rkt
@@ -4,14 +4,22 @@
 
 ; TASK:
 ;
-; Золотое сечение
+; Поиск неподвижной точки
 ;
-; Покажите, что золотое сечение φ (раздел 1.2.2) есть неподвижная точка
-; трансформации x → 1 + 1/x , и используйте этот факт для вычисления
-; φ с помощью процедуры fixed-point .
+; Измените процедуру fixed-point так, чтобы она печатала последовательность
+; приближений, которые порождает, с помощью примитивов newline и display
+; , показанных в упражнении 1.22 .
+; Затем найдите решение уравнения x^x = 1000 путем поиска неподвижной точки
+; x → log(1000)/log(x) .
+; (Используйте встроенную процедуру Scheme log , которая вычисляет
+; натуральные логарифмы.)
+; Посчитайте, сколько шагов это занимает при использовании торможения
+; усреднением и без него.
+; (Учтите, что нельзя начинать fixed-point со значения 1, поскольку это
+; вызовет деление на log(1) = 0 .)
 
 (define (fixed-point f first-guess)
-  (define tolerance 0.0000000001)
+  (define tolerance 0.000001)
   (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
 
   (define (try guess)
@@ -23,17 +31,22 @@
   (try first-guess))
 
 
-
 ; SOLUTION:
 
-; Формула золотого сечения:
-; φ^2 = φ + 1
-;
-; Поделим обе части на φ:
-; φ = 1 + 1/φ
-;
-; ЧТД:   x -> 1 + 1/x
+(define (fixed-point f first-guess)
+  (define tolerance 0.000001)
+  (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
 
-(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
 
+  (try first-guess))
 
+#| (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0) |#
+
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 1.1)
